e3f4e911e99e386e9df117bda54fd82d9e1fb728
troat9824 <troat9824@gmail.com>
2022-03-07 06:24:47 -0600

    starting on making containers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 var myKey = config.My_Key;
 var userFormEl = document.querySelector("#user-form");
-var cityNameInputEl = document.querySelector("#city-name")
+var cityNameInputEl = document.querySelector("#city-name");
+var citySearchTerm = document.querySelector(".city-search-term");
+var daysContainer = document.querySelector("#days-container");
+
 
 
 var formSubmitHandler = function(event) {
@@ -11,7 +14,7 @@ var formSubmitHandler = function(event) {
 
     if (cityName) {
         getWeather(cityName);
-        cityName.value = "";
+        cityNameInputEl.value = "";
     } else {
         alert("Please enter a City name");
     };
@@ -21,15 +24,46 @@ var formSubmitHandler = function(event) {
 
 getWeather = function(cityName) {
     // weather app url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + myKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + myKey;
 
     // make request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayWeather(data, cityName);
         });
     });
 
 };
+
+// not working
+displayWeather = function(weather, searchTerm) {
+    // clear old content
+    daysContainer.textContent = "";
+    citySearchTerm.textContent = searchTerm;
+
+    console.log(searchTerm);
+    console.log(weather);
+
+    for (var i = 0; i < weather.length; i++) {
+        // format city name
+        var cityWeather = weather[i].name + weather[i].main.temp;
+
+
+        //create a container for each repo
+
+        var weatherEl = document.createElement("div");
+        weatherEl.classList = "col-12";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = cityWeather;
+
+        // append to container
+        weatherEl.appendChild(titleEl);
+
+        // append container to the dom
+        daysContainer.appendChild(weatherEl);
+    }
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
