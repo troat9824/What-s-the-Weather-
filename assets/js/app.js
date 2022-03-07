c52905c3af6f13b8a1705412d27f06e0d1496ca0
troat9824 <troat9824@gmail.com>
2022-03-07 04:28:18 -0600

    fetch url

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
 var myKey = config.My_Key;
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+getWeather = function() {
+    // weather app url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + myKey;
+
+    // make request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+};
+
+getWeather();
