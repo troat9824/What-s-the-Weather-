b5b2e19bcbc5aa50736163e5ff5acf9f9db1e37e
troat9824 <troat9824@gmail.com>
2022-03-07 16:38:55 -0600

    some CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,9 @@ var userFormEl = document.querySelector("#user-form");
 var cityNameInputEl = document.querySelector("#city-name");
 var citySearchTerm = document.querySelector(".city-search-term");
 var daysContainerUl = document.querySelector(".days-container-ul");
-var currentWeatherContainer = document.querySelector(".currentWeatherContainer")
-var daysContainer = document.querySelector("#days-container")
+var currentWeatherContainer = document.querySelector(".currentWeatherContainer");
+var daysContainer = document.querySelector("#days-container");
+var lineBreak = document.createElement("br");
 
 
 var formSubmitHandler = function(event) {
@@ -20,6 +21,15 @@ var formSubmitHandler = function(event) {
         alert("Please enter a City name");
     };
 };
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
 // for weather
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
@@ -53,14 +63,21 @@ getWeather = function(cityName) {
 // not working
 displayWeather = function(weather, searchTerm) {
     // clear old content
-    var forecastDayDiv = document.createElement("div");
-    var forecastDayP = document.createElement("p");
+    searchTerm = toTitleCase(searchTerm);
+
     currentWeatherContainer.innerHTML = "";
     daysContainer.innerHTML = "";
-    citySearchTerm.textContent = searchTerm;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    citySearchTerm.textContent = searchTerm + " " + today;
 
     var subtitle = document.createElement("h2")
     subtitle.innerHTML = "5 Day Forecast: ";
+    subtitle.setAttribute("class", "col-12 text-center")
     daysContainer.appendChild(subtitle);
 
     console.log(searchTerm);
@@ -68,15 +85,16 @@ displayWeather = function(weather, searchTerm) {
     // current temperature
     var currentTemp = weather.current.temp;
     var currentTempEl = document.createElement("p");
-    currentTempEl.classList = "col-12";
+    currentTempEl.classList = "text-start";
     currentTempEl.innerHTML = "Temperature: " + currentTemp + " degrees F";
 
     currentWeatherContainer.appendChild(currentTempEl);
 
     // current description
     var currentDesc = weather.current.weather[0].description;
+    currentDesc = toTitleCase(currentDesc);
     var currentDescEl = document.createElement("p");
-    currentDescEl.classList = "col-12";
+    currentDescEl.classList = "text-start";
     currentDescEl.textContent = currentDesc;
 
     currentWeatherContainer.appendChild(currentDescEl);
@@ -84,7 +102,7 @@ displayWeather = function(weather, searchTerm) {
     // current humidity
     var currentHumidity = weather.current.humidity;
     var currentHumidityEl = document.createElement("p");
-    currentHumidityEl.classList = "col-12";
+    currentHumidityEl.classList = "text-start";
     currentHumidityEl.innerHTML = "Humidity: " + currentHumidity;
 
     currentWeatherContainer.appendChild(currentHumidityEl);
@@ -92,7 +110,7 @@ displayWeather = function(weather, searchTerm) {
     // current wind speed
     var currentWindSpeed = weather.current.wind_speed;
     var currentWindSpeedEl = document.createElement("p");
-    currentWindSpeedEl.classList = "col-12";
+    currentWindSpeedEl.classList = "text-start";
     currentWindSpeedEl.innerHTML = "Wind Speed: " + currentWindSpeed;
 
     currentWeatherContainer.appendChild(currentWindSpeedEl);
@@ -100,7 +118,7 @@ displayWeather = function(weather, searchTerm) {
     // current UV index
     var currentUvi = weather.current.uvi;
     var currentUviEl = document.createElement("p");
-    currentUviEl.classList = "col-12";
+    currentUviEl.classList = "text-start";
     currentUviEl.innerHTML = "UV Index: " + currentUvi;
 
     currentWeatherContainer.appendChild(currentUviEl);
@@ -108,8 +126,21 @@ displayWeather = function(weather, searchTerm) {
     // current UV Index color change
 
     for (var i = 1; i < 6; i++) {
-        
+        var forecastDayDiv = document.createElement("div");
+        var forecastDayP = document.createElement("p");
         var forecastDayUl = document.createElement("ul");
+        
+
+        var tomorrow = new Date();
+        var dd = String(tomorrow.getDate() + i).padStart(2, '0');
+        var mm = String(tomorrow.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = tomorrow.getFullYear();
+        tomorrow = mm + '/' + dd + '/' + yyyy;
+        var futureDateEl = document.createElement("li");
+        futureDateEl.innerHTML = tomorrow;
+
+    
+        
 
         // forecast temp
         var forecastTemp = weather.daily[i].temp.day;
@@ -120,6 +151,7 @@ displayWeather = function(weather, searchTerm) {
 
         // forecast desc
         var forecastDesc = weather.daily[i].weather[0].description;
+        forecastDesc = toTitleCase(currentDesc);
         weatherElDesc = document.createElement("li");
         weatherElDesc.innerHTML = forecastDesc;
 
@@ -141,16 +173,23 @@ displayWeather = function(weather, searchTerm) {
         // append container to the dom
 
         // add all elements together to day ul
+        forecastDayUl.appendChild(futureDateEl);
         forecastDayUl.appendChild(weatherElDesc);
         forecastDayUl.appendChild(weatherElTemp);
         forecastDayUl.appendChild(weatherElHum);
         forecastDayUl.appendChild(weatherElWindSpeed);
         forecastDayUl.appendChild(weatherElUvi);
 
+
+
         // add ul to container
         forecastDayP.appendChild(forecastDayUl);
         forecastDayDiv.appendChild(forecastDayP);
         daysContainer.appendChild(forecastDayDiv);
+
+        forecastDayDiv.setAttribute("class", "card flex-wrap justify-content-space-between")
+        forecastDayP.setAttribute("class", "card-body flex dayP")
+        forecastDayUl.setAttribute("class", "card-text text-center flex-wrap col-2 justify-content-space-between")
     };
 
 }
